2e38b948688eb7b5c9c71d9dfde1fea3546e707a
Collin Rea <collinr01@gmail.com>
2018-02-23 06:37:52 -0500

    Change name and add parseMessage function Not able to handle strings with just one word at this time

---
+++ Lesson2/LogAnalysis.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage s
+  | x == "I" = LogMessage Info fstInt (unwords xs)
+  | x == "W" = LogMessage Warning fstInt (unwords xs)
+  | x == "E" = LogMessage (Error fstInt) (read (head xs) :: Int) (unwords $ tail xs)
+  | otherwise = Unknown s
+  where 
+        (x:y:xs) = words s
+        fstInt = read y :: Int
