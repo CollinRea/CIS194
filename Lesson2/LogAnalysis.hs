76a987f6b82bf2d29be7fcb6535f7e10c6309a3b
Collin Rea <collinr01@gmail.com>
2018-02-23 20:25:20 -0500

    Add insert function for new Binary tree

--- Lesson2/LogAnalysis.hs
+++ Lesson2/LogAnalysis.hs
@@ -3,6 +3,9 @@ module LogAnalysis where
 
 import Log
 
+-- Exercise 1
+
+-- Parse individual String messages
 parseMessage :: String -> LogMessage
 parseMessage s
   | x == "I" = LogMessage Info fstInt (unwords xs)
@@ -13,6 +16,20 @@ parseMessage s
         (x:y:xs) = words s
         fstInt = read y :: Int
 
+-- Parse a multi-line string and return list of LogMessage(s)
 parse :: String -> [LogMessage]
 parse "" = []
 parse logFile = map parseMessage (lines logFile)
+
+
+-- Exercise 2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert m Leaf = Node Leaf m Leaf
+insert m (Node left (Unknown _) right) = Node left m right
+insert m@(LogMessage _ x _) bt@(Node left n@(LogMessage _ y _) right)
+    | x < y     = Node (insert m left) n right
+    | x > y     = Node left n (insert m right)
+    | otherwise = bt
+
