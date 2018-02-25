c6a2d92a12f3ba1bf068776fa7337ca3ce17e1a4
Collin Rea <collinr01@gmail.com>
2018-02-25 06:36:44 -0500

    Complete exercise 2

--- Lesson3/CodeGolf.hs
+++ Lesson3/CodeGolf.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module CodeGolf where
 
-import Data.List
+-- import Data.List
 
 -- Exercise 1 Hopscotch
 
@@ -19,3 +19,16 @@ every n xs =
   case drop (n-1) xs of
     [] -> []
     (y:ys) -> y : every n ys
+
+
+-- Exercise 2 Local maxima
+
+-- If less than 3 in list, return empty list
+-- Need to be able to compare the middle element
+-- localMaxima [2,9,5,6,1] == [9,6]
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs | length xs < 3 = []
+localMaxima l@(x:y:z:_)
+  | x < y && y > z = y : next
+  | otherwise = next
+  where next = localMaxima (drop 1 l)
