1ff0446f5f1f8c1cb3e4c92e236b315384a7dbeb
Collin Rea <collinr01@gmail.com>
2018-02-25 12:09:10 -0500

    Complete Exercise 3

--- Lesson3/CodeGolf.hs
+++ Lesson3/CodeGolf.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module CodeGolf where
 
--- import Data.List
+import Data.List
 
 -- Exercise 1 Hopscotch
 
@@ -32,3 +32,40 @@ localMaxima l@(x:y:z:_)
   | x < y && y > z = y : next
   | otherwise = next
   where next = localMaxima (drop 1 l)
+
+
+-- Exercise 3 Histogram
+
+-- Histogram takes the first list of Integers and creates
+-- a list of Tuples, with the Number and how many there are using 
+-- elemIndices to count the numbers in the list
+histogram :: [Integer] -> String
+histogram [] = "==========\n0123456789\n"
+histogram xs = concat (visualize list ++ [histogram []])
+  where
+    list = [(n, length (elemIndices n xs)) | n <- [0..9]]
+
+-- This function takes the list of tuples and builds the list of rows
+-- Uses helper function convertRow to draw the "*" and " "
+-- Also uses decr local function to decrement the 2nd number in the tuple
+-- as we draw out each row
+visualize :: [(Integer, Int)] -> [String]
+visualize [] = []
+visualize xs
+  | any (\(_,n)-> n > 0) xs = visualize (decr xs) ++ convertRow xs
+  | otherwise = []
+  where
+    decr = map (\(x,n)->(x,n-1))
+
+-- Takes a list of Tuples and converts to list of strings
+convertRow :: [(Integer, Int)] -> [String]
+convertRow l = (map (\(_,n)-> if n > 0 then "*" else " ") l) ++ ["\n"]
+
+
+-----------------------------------------------------------------
+-- Didn't use this function but keeping it for reference
+
+-- Replace String at given index in list and return new list
+insertAt :: Int -> String-> [String] -> [String] 
+insertAt z y xs = as ++ (y : bs)
+  where (as,(_:bs)) = splitAt z xs
