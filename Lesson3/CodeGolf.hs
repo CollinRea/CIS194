c2c9552d2cdc422b29a2cbd22bbc0063a818d259
Collin Rea <collinr01@gmail.com>
2018-02-25 06:09:00 -0500

    Correction to first exercise

--- Lesson3/CodeGolf.hs
+++ Lesson3/CodeGolf.hs
@@ -1,10 +1,21 @@
 {-# OPTIONS_GHC -Wall #-}
 module CodeGolf where
 
+import Data.List
 
 -- Exercise 1 Hopscotch
 
+-- Take a list, and return a list of lists
+-- taking every n + 1 starting at 1 until the length of the list.
+-- ie:  skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
 skips :: [a] -> [[a]]
-skips [] = [[]]
-skips [x] = [[x]]
-skips l@(_:xs) = l : (skips xs)
+skips xs = [every n xs | n <- [1..len]]
+  where len = length xs
+
+-- Helper function to use in skips, takes a number and a list
+-- returns a new list of every N in the list
+every :: Int -> [a] -> [a]
+every n xs = 
+  case drop (n-1) xs of
+    [] -> []
+    (y:ys) -> y : every n ys
