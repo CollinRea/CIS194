edab906b28fde3bcb0947cb11e7f98239315470e
Collin Rea <collinr01@gmail.com>
2018-02-28 06:30:28 -0500

    Work in progress to be able to insert a tree, still need to increment the Height correctly when inserting into a node

--- Lesson4/Lesson4.hs
+++ Lesson4/Lesson4.hs
@@ -25,3 +25,36 @@ fun2 n
 -- 'Wholemeal' version
 fun2' :: Integer -> Integer 
 fun2' =  sum . filter even . takeWhile (>1) . iterate (\x -> if odd x then 3 * x + 1 else x `div` 2)
+
+
+-- Exercise 2 Folding with trees
+
+-- Notes
+data Tree a 
+  = Leaf
+  | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+-- foldTree :: [a] -> Tree a
+-- foldTree = 
+
+insert :: Eq a => a -> Tree a -> Tree a
+insert v Leaf = Node 0 Leaf v Leaf
+insert v t@(Node i left x right)
+  | findT == left = Node newHeight (insert v left) x right
+  | findT == right = Node newHeight left x (insert v right)
+  where findT = compareHeight t
+        newHeight = 
+          if left == Leaf && right == Leaf then
+            i
+          else 
+            i + 1 
+
+compareHeight :: Tree a -> Tree a
+compareHeight Leaf = Leaf
+compareHeight (Node 0 left _ _) = left
+compareHeight (Node _ l@(Leaf) _ _) = l
+compareHeight (Node _ (Node _ _ _ _) _ r@(Leaf)) = r
+compareHeight (Node _ l@(Node i _ _ _) _ r@(Node u _ _ _))
+  | i <= u = l
+  | otherwise = r 
