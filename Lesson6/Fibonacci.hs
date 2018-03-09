bde16c5c7aaddbbf51746f994ff972391329c0b5
Collin Rea <collinr01@gmail.com>
2018-03-08 21:35:52 -0500

    Exercise 1 and part of 2

--- Lesson6/Fibonacci.hs
+++ Lesson6/Fibonacci.hs
@@ -2,3 +2,22 @@
 
 module Fibonacci where
 
+import Data.List
+
+-- Exercise 1
+
+-- Fibonacci number for given integer
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+-- Infinite list of all Fib numbers
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- Exercise 2
+
+-- Faster Fibonacci sequence
+fibs2 :: [Integer]
+fibs2 = foldl' (\x y -> (x ++ [fib y])) [] (take 20 [0..])
