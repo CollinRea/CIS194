7411e56d3f46f1da9ff1490fae7adb053caf7573
Collin Rea <collinr01@gmail.com>
2018-03-09 05:56:40 -0500

    Faster Fibonacci sequence using lazy evaluation

--- Lesson6/Fibonacci.hs
+++ Lesson6/Fibonacci.hs
@@ -2,8 +2,6 @@
 
 module Fibonacci where
 
-import Data.List
-
 -- Exercise 1
 
 -- Fibonacci number for given integer
@@ -16,8 +14,20 @@ fib n = fib (n - 1) + fib (n - 2)
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
+
 -- Exercise 2
 
--- Faster Fibonacci sequence
+-- Faster Fibonacci sequence starts with first 2 numbers, then uses next function
+-- to add them and call next again recursively forever
+-- The really cool part that you may notice is we pass the tail to next, and due
+-- to lazy evaluation, it just keeps going without actually knowing the number until
+-- it needs it.
 fibs2 :: [Integer]
-fibs2 = foldl' (\x y -> (x ++ [fib y])) [] (take 20 [0..])
+fibs2 = [0,1] ++ next fibs2
+  where
+    next (x : xs@(y:_)) = (x + y) : next xs
+
+
+-- Exercise 3
+
+-- Streams
