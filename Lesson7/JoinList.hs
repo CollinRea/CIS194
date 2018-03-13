ab1534a874daab3805ce4f9d90cab3550beed0ad
Collin Rea <collinr01@gmail.com>
2018-03-12 21:26:29 -0400

    Append function for Exercise 1

--- Lesson7/JoinList.hs
+++ Lesson7/JoinList.hs
@@ -2,7 +2,17 @@ module JoinList where
 
 
 data JoinList m a = Empty
-  | Single m a
-  | Append m (JoinList m a) (JoinList m a)
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
 
+-- Exercise 1 
+
+-- Append function or JoinList
+(+++) :: Monoid m => JoinList m a  -> JoinList m a -> JoinList m a
+(+++) l1 l2 = Append (tag l1 `mappend` tag l2) l1 l2
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
