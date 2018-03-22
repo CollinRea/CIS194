b3a3d66ed5ea24f7c0bb19b81ecc3827fb728632
Collin Rea <collinr01@gmail.com>
2018-03-21 21:02:40 -0400

    Exercise 4 WIP - seems to work but trying to find a better way

--- Lesson8/Party.hs
+++ Lesson8/Party.hs
@@ -27,7 +27,8 @@ moreFun gl1 gl2
 
 -- Rose Tree Fold function
 treeFold :: (a -> [b] -> b) -> Tree a -> b
-treeFold f (Node rv ts) = f rv $ map (treeFold f) ts
+treeFold f = go where
+    go (Node x ts) = f x (map go ts)
 
 
 -- Exercise 3
@@ -36,3 +37,21 @@ treeFold f (Node rv ts) = f rv $ map (treeFold f) ts
 -- Combine guest lists (1st Pair is with bosses, 2nd pair is without)
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
 nextLevel b gls = foldr mappend (b `glCons` mempty,mempty) gls
+
+
+-- Exercise 4
+
+-- get Guests Lists (without , with boss)
+guestLists :: Tree Employee -> (GuestList, GuestList)
+guestLists (Node e []) = (mempty, GL [e] $ empFun e)
+guestLists (Node e es) =
+  (mconcat $ map (uncurry moreFun) $ withBoss es,
+  GL [e] (empFun e) `mappend` (mconcat $ withoutBoss es))
+  where withBoss es = map guestLists es
+        withoutBoss es = map fst $ withBoss es
+
+
+-- Find max Fun Guest List given a Tree of Employees
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . guestLists
+
