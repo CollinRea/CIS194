412537eec64db73e48ca1866a6a340f984c9794d
Collin Rea <collinr01@gmail.com>
2018-03-22 20:21:07 -0400

    Todo command line program

---
+++ Lesson8/todo.hs
@@ -0,0 +1,45 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+-- List of actions that can be taken as first Command line argument
+dispatch :: [(String, [String] -> IO ())]
+dispatch = 
+  [ ("add", add)
+  , ("view", view)
+  , ("remove", remove)
+  ]
+
+-- Interact with the world
+main :: IO ()
+main = do
+  (command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
+
+-- Action Functions
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, num] = do  
+  handle <- openFile fileName ReadMode  
+  (tempName, tempHandle) <- openTempFile "." "temp"  
+  contents <- hGetContents handle  
+  let number = read num  
+      todoTasks = lines contents  
+      newTodoItems = delete (todoTasks !! number) todoTasks  
+  hPutStr tempHandle $ unlines newTodoItems  
+  hClose handle  
+  hClose tempHandle  
+  removeFile fileName  
+  renameFile tempName fileName  
