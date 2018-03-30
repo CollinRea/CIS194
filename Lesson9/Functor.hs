48a48737fd9ba53203f8c6482711792e383b2b65
Collin Rea <collinr01@gmail.com>
2018-03-29 20:57:47 -0400

    Functor instance for a Tuple

--- Lesson9/Functor.hs
+++ Lesson9/Functor.hs
@@ -2,6 +2,8 @@
 
 module Functor where
 
+import GHC.Base (ignore Data.Tuple)
+
 data Either' e a = Left' e | Right' a
   deriving (Eq, Ord, Read, Show)
 
@@ -9,4 +11,8 @@ instance Functor (Either' e) where
   fmap :: (a -> b) -> Either' e a -> Either' e b
   fmap _ (Left' e) = Left' e
   fmap g (Right' x) = Right' (g x)
-  
+ 
+-- Instance of Functor for a Tuple
+--instance Functor ((,) e) where
+--  fmap :: (a -> b) -> (e,a) -> (e,b)
+--  fmap g (e,x) = (e,(g x))
