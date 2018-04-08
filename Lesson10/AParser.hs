12971b8c5cbeb090957320819766dbecf875c95e
Collin Rea <collinr01@gmail.com>
2018-04-07 20:17:16 -0400

    Functor instance for Parser

--- Lesson10/AParser.hs
+++ Lesson10/AParser.hs
@@ -54,7 +54,11 @@ posInt = Parser f
 -- Your code goes below here
 ------------------------------------------------------------
 
+-- Helper function for Parser Functor instance
 first :: (a -> b) -> (a,c) -> (b,c)
 first f (x,y) = (f x,y)
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser (fmap (first f) . p)
 
 
